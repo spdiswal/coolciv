56137dba7d5f4737f0d4dc4cd2cb72c795853b0f
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2022-11-23 16:17:16 +0100

    wip! Implement AlphaCiv

--- src/alphaciv.spec.ts
+++ src/alphaciv.spec.ts
@@ -1,12 +1,77 @@
 import { composeAlphaCivGame } from "./game"
 
-describe("an AlphaCiv game", () => {
-	it("begins in 4000 BCE", () => {
-		// GIVEN an AlphaCiv game.
-		const game = composeAlphaCivGame()
+// TODO: The world age remains 4000 BCE after Red completes their first turn.
+// TODO: After turn 2, the world age is 3800 BCE.
+// TODO: ..................................................
 
-		// THEN the current year is 4000 BCE initially.
-		const expectedYear = "4000 BCE"
-		expect(game.currentYear).toBe(expectedYear)
+describe("an AlphaCiv game", () => {
+	describe("world age", () => {
+		it("begins in 4000 BCE", () => {
+			// GIVEN an AlphaCiv game.
+			const game = composeAlphaCivGame()
+			
+			// THEN the current year is 4000 BCE initially.
+			const expectedYear = "4000 BCE"
+			expect(game.currentYear).toBe(expectedYear)
+		})
+		
+		it("the world age is 3900 BCE after Blue completes their first turn", () => {
+			// GIVEN
+			const game = composeAlphaCivGame()
+			
+			// WHEN
+			game.endTurn()
+			game.endTurn()
+			
+			// THEN
+			expect(game.currentYear).toBe("3900 BCE")
+		})
+		
+		it("the world age is 3800 BCE after turn 2", () => {
+			// GIVEN
+			const game = composeAlphaCivGame()
+			
+			// WHEN
+			game.endTurn()
+			game.endTurn()
+			
+			// THEN
+			expect(game.currentYear).toBe("3800 BCE")
+		})
+	})
+
+	describe("players", () => {
+		it("begins with Red player in turn", () => {
+			// GIVEN
+			const game = composeAlphaCivGame()
+			
+			// THEN
+			expect(game.currentPlayer).toBe("Red")
+		})
+		
+		it("puts Blue player in turn after Red completes their turn", () => {
+			const game = composeAlphaCivGame()
+			expect(game.currentPlayer).toBe("Red")
+			
+			// WHEN
+			game.endTurn()
+			
+			// THEN
+			expect(game.currentPlayer).toBe("Blue")
+		})
+		
+		it("puts Red player in turn after Blue completes their turn", () => {
+			// GIVEN
+			const game = composeAlphaCivGame()
+			game.endTurn()
+			
+			expect(game.currentPlayer).toBe("Blue")
+			
+			// WHEN
+			game.endTurn()
+			
+			// THEN
+			expect(game.currentPlayer).toBe("Red")
+		})
 	})
 })
--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,31 @@
 export type Game = {
+	readonly currentPlayer: string
 	readonly currentYear: string
+	readonly endTurn: () => void
 }
 
+const playerRed = "Red"
+const playerBlue = "Blue"
+
 export function composeAlphaCivGame(): Game {
+	let currentPlayer = playerRed
+	let currentYear = "4000 BCE"
+	
 	return {
-		currentYear: "",
+		get currentPlayer() {
+			return currentPlayer
+		},
+		get currentYear() {
+			return currentYear
+		},
+		endTurn: () => {
+			if (currentPlayer === playerRed) {
+				currentPlayer = playerBlue
+			} else {
+				currentPlayer = playerRed
+			}
+			
+			currentYear = "3900 BCE"
+		}
 	}
 }
